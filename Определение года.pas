7fb924d81936311d84f456419f48baddae1c5160
DanilK17 <danil.17.4.2004@gmail.com>
2022-10-10 21:59:05 +0300

    Update определение года(добавлено определение секунд в минуте)

--- Определение года.pas
+++ Определение года.pas
@@ -1,8 +1,8 @@
 ﻿begin
-  var x := readinteger('Введите год');
+  var x := readinteger('Введите год:');
   if x div 4 = 0 then
     print('Год високосный');
   else print('Год не високосный');
-  
-  
+  var y := readreal('Введите кол-во минут:')
+  print('Кол-во секунд:',y*60);
 end.
